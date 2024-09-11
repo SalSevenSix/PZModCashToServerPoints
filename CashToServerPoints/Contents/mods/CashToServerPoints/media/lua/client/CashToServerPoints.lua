ac45376b3cb8622b42694d288744e68f4009dc85
Bowden Salis <bsalis76@gmail.com>
2024-09-11 15:17:44 +0700

    Added recipe to convert 100 cash to points

--- CashToServerPoints/Contents/mods/CashToServerPoints/media/lua/client/CashToServerPoints.lua
+++ CashToServerPoints/Contents/mods/CashToServerPoints/media/lua/client/CashToServerPoints.lua
@@ -5,6 +5,10 @@ function CashToServerPoints.GiveServerPoints10()
   sendClientCommand("ServerPoints", "add", {getPlayer():getUsername(), 10})
 end
 
+function CashToServerPoints.GiveServerPoints100()
+  sendClientCommand("ServerPoints", "add", {getPlayer():getUsername(), 100})
+end
+
 function CashToServerPoints.AddExtraRandomMoney()
   getPlayer():getInventory():AddItems("Base.Money", ZombRand(SandboxVars.CashToServerPoints.WalletMoneyLimit or 3.0))
 end
